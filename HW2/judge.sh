28fec7ea321b7a84f660cb6a5fbd4fe28edd7427
jengku <jengku@gmail.com>
2018-10-19 10:07:12 +0800

    compatible with freebsd/macos

--- HW2/judge.sh
+++ HW2/judge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OUTPUT1="$(mktemp -t oj-test-input-1-XXXXXX)"
 OUTPUT2="$(mktemp -t oj-test-input-2-XXXXXX)"
-tee >(./rsort -m 512 > "$OUTPUT1")  >(sort --parallel=7 > "$OUTPUT2") > /dev/null
+tee >(./rsort -m 512 > "$OUTPUT1")  >(sort > "$OUTPUT2") > /dev/null
 diff "$OUTPUT1" "$OUTPUT2" -q &> /dev/null
 RET=$?
 rm -f "$OUTPUT1"
