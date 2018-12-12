4f9de55504f2dfb297d8c3214187d8f0b1c376eb
peter0749 <jengku@gmail.com>
2018-12-12 18:18:13 +0800

    import float.h

--- FINAL_PJ/kmeans_test/kmeans.h
+++ FINAL_PJ/kmeans_test/kmeans.h
@@ -4,12 +4,13 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
-#ifndef DBL_MAX
-#define DBL_MAX 2.0e9
-#endif
-#ifndef DBL_MIN
-#define DBL_MIN -2.0e9
-#endif
+#include <float.h>
+//#ifndef DBL_MAX
+//#define DBL_MAX 2.0e9
+//#endif
+//#ifndef DBL_MIN
+//#define DBL_MIN -2.0e9
+//#endif
 double hist_intersection(unsigned int *A, double *B, unsigned int cols);
 void *kmeans_intersec_int(unsigned int **data, unsigned int **return_labels, double ***return_centroid, int rows, int cols, int K, double tol);
 double hist_intersection_f(double *A, double *B, unsigned int cols);
